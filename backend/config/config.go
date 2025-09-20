2165f648d9420777eb5d32fe95956c73f513cbd7
goesbams <goesbamsday@gmail.com>
2025-09-20 17:03:55 +0700

    create config files

---
+++ backend/config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"io/ioutil"
+	"log"
+
+	"gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	Database struct {
+		User     string `yaml:"user"`
+		Password string `yaml:"password"`
+		Host     string `yaml:"host"`
+		Port     string `yaml:"port"`
+		Dbname   string `yaml:"dbname"`
+		Sslmode  string `yaml:"sslmode"`
+	} `yaml:"database"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	config := &Config{}
+
+	file, err := ioutil.ReadFile(path)
+	if err != nil {
+		log.Fatalf("error reading config file: %v", err)
+		return nil, err
+	}
+
+	err = yaml.Unmarshal(file, config)
+	if err != nil {
+		log.Fatalf("error unmarshalling yaml: %v", err)
+		return nil, err
+	}
+
+	return config, nil
+}
--- backend/go.mod
+++ backend/go.mod
@@ -2,8 +2,4 @@ module github.com/goesbams/mini-books-library/backend
 
 go 1.25.1
 
-require (
-	github.com/golang-migrate/migrate/v4 v4.19.0 // indirect
-	github.com/hashicorp/errwrap v1.1.0 // indirect
-	github.com/hashicorp/go-multierror v1.1.1 // indirect
-)
+require gopkg.in/yaml.v2 v2.4.0
