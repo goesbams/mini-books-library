f1330f0cc35821506b60f3f1eaca6a7ab9026a07
goesbams <goesbamsday@gmail.com>
2025-09-21 11:08:32 +0700

    using sqlx for simplify and enhance database interaction code

--- backend/database/database.go
+++ backend/database/database.go
@@ -1,15 +1,15 @@
 package database
 
 import (
-	"context"
 	"fmt"
 	"log"
 
 	"github.com/goesbams/mini-books-library/backend/config"
-	"github.com/jackc/pgx/v5"
+	"github.com/jmoiron/sqlx"
+	_ "github.com/lib/pq"
 )
 
-func ConnectDB(cfg *config.Config) (*pgx.Conn, error) {
+func ConnectDB(cfg *config.Config) (*sqlx.DB, error) {
 	connStr := fmt.Sprintf("postgres://%s:%s@%s:%d/%s?sslmode=%s",
 		cfg.Database.User,
 		cfg.Database.Password,
@@ -19,13 +19,13 @@ func ConnectDB(cfg *config.Config) (*pgx.Conn, error) {
 		cfg.Database.Sslmode,
 	)
 
-	conn, err := pgx.Connect(context.Background(), connStr)
+	conn, err := sqlx.Connect("postgres", connStr)
 	if err != nil {
 		log.Fatal("unable to connect to database:", err)
 		return nil, err
 	}
 
-	err = conn.Ping(context.Background())
+	err = conn.Ping()
 	if err != nil {
 		log.Fatal("failed to ping database")
 		return nil, err
